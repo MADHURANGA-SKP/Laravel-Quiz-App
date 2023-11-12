5e0373dc4926911e17142ab7ee3de1197c567867
Pasan Madhuranga <121794032+ISE-EXPLORELOGY@users.noreply.github.com>
2023-11-12 14:23:16 +0530

    feat:login register

---
+++ vue/src/components/AuthLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+  >
+    <router-view></router-view>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,7 +1,12 @@
 import { createStore } from "vuex";
 
 const store = createStore({
-  state: {},
+  state: {
+    user: {
+      data: {},
+      token: 123,
+    },
+  },
   getters: {},
   actions: {},
   mutations: {},
