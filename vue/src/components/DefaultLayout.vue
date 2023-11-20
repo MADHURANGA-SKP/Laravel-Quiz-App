dbd1fb550f6e35f40399719fae58f9d5eb97f663
Pasan Madhuranga <121794032+ISE-EXPLORELOGY@users.noreply.github.com>
2023-11-20 11:55:51 +0530

    feat:latest-update-of-logout-buttom

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -31,15 +31,6 @@
           </div>
           <div class="hidden md:block">
             <div class="ml-4 flex items-center md:ml-6">
-              <button
-                type="button"
-                class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-              >
-                <span class="absolute -inset-1.5" />
-                <span class="sr-only">View notifications</span>
-                <BellIcon class="h-6 w-6" aria-hidden="true" />
-              </button>
-
               <!-- Profile dropdown -->
               <Menu as="div" class="relative ml-3">
                 <div>
@@ -66,18 +57,13 @@
                   <MenuItems
                     class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                   >
-                    <MenuItem
-                      v-for="item in userNavigation"
-                      :key="item.name"
-                      v-slot="{ active }"
-                    >
+                    <MenuItem v-slot="{ active }">
                       <a
-                        :href="item.href"
+                        @click="logout"
                         :class="[
-                          active ? 'bg-gray-100' : '',
-                          'block px-4 py-2 text-sm text-gray-700',
+                          'block px-4 py-2 text-sm text-gray-700 cursor-pointer',
                         ]"
-                        >{{ item.name }}</a
+                        >Sign Out</a
                       >
                     </MenuItem>
                   </MenuItems>
@@ -132,23 +118,13 @@
                 {{ user.email }}
               </div>
             </div>
-            <button
-              type="button"
-              class="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-            >
-              <span class="absolute -inset-1.5" />
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
           </div>
           <div class="mt-3 space-y-1 px-2">
             <DisclosureButton
-              v-for="item in userNavigation"
-              :key="item.name"
+              @click="logout"
               as="a"
-              :href="item.href"
-              class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-              >{{ item.name }}</DisclosureButton
+              class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white cursor-pointer"
+              >Sign Out</DisclosureButton
             >
           </div>
         </div>
@@ -173,8 +149,10 @@ import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
 import { useStore } from "vuex";
 import { computed } from "vue";
 import store from "../store";
+import { useRouter } from "vue-router";
 
 const storex = useStore();
+const router = useRouter();
 
 const navigation = [
   { name: "Dashboard", to: { name: "Dashboard" } },
@@ -187,4 +165,11 @@ const userNavigation = [
 ];
 
 const user = computed(() => store.state.user.data);
+
+function logout() {
+  store.commit("logout");
+  router.push({
+    name: "Login",
+  });
+}
 </script>
