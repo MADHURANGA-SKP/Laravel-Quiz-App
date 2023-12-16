a7b657e5a09b4619c3d3a7b16262fee54dbaa9d0
Pasan Madhuranga <121794032+ISE-EXPLORELOGY@users.noreply.github.com>
2023-12-16 23:55:10 +0530

    latext-changes-survey-page

---
+++ src/components/PageComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+        {{ title }}
+      </h1>
+      <slot name="header"></slot>
+    </div>
+  </header>
+  <main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <div class="px-4 py-6 sm:px-0">
+        <slot></slot>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { stringifyQuery } from "vue-router";
+
+const props = defineProps({
+  title: String,
+});
+</script>
+
+<style scoped></style>
